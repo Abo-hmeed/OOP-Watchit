1d1e234518564927c198b37357a96018b91f3cc5
Abdo-Zikry <151969450+Abdo-Zikry@users.noreply.github.com>
2023-12-21 16:16:43 +0200

    Update IOClass.java

--- IOClass.java
+++ IOClass.java
@@ -2,34 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class IOClass {
-
-    public static void OutputData() throws IOException {
-        File file = new File("adminList.txt");
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-        oos.writeObject(AdminList.arrAdmin);
-        oos.close();
-
-        file = new File("movieList.txt");
-        oos = new ObjectOutputStream(new FileOutputStream(file));
-        oos.writeObject(MovieList.allMovies);
-        oos.close();
-
-        file = new File("castList.txt");
-        oos = new ObjectOutputStream(new FileOutputStream(file));
-        oos.writeObject(CastList.allActors);
-        oos.close();
-
-        file = new File("userList.txt");
-        oos = new ObjectOutputStream(new FileOutputStream(file));
-        oos.writeObject(UserList.arr);
-        oos.close();
-
-        file = new File("directorList.txt");
-        oos = new ObjectOutputStream(new FileOutputStream(file));
-        oos.writeObject(DirectorList.directorsList);
-        oos.close();
-    }
-
+    //Inputs data from files to program
     public static void InputData() throws IOException, ClassNotFoundException {
         File file = new File("adminList.txt");
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
@@ -57,4 +30,31 @@ public class IOClass {
         ois.close();
     }
 
+    //Outputs data from program into files
+    public static void OutputData() throws IOException {
+        File file = new File("adminList.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+        oos.writeObject(AdminList.arrAdmin);
+        oos.close();
+
+        file = new File("movieList.txt");
+        oos = new ObjectOutputStream(new FileOutputStream(file));
+        oos.writeObject(MovieList.allMovies);
+        oos.close();
+
+        file = new File("castList.txt");
+        oos = new ObjectOutputStream(new FileOutputStream(file));
+        oos.writeObject(CastList.allActors);
+        oos.close();
+
+        file = new File("userList.txt");
+        oos = new ObjectOutputStream(new FileOutputStream(file));
+        oos.writeObject(UserList.arr);
+        oos.close();
+
+        file = new File("directorList.txt");
+        oos = new ObjectOutputStream(new FileOutputStream(file));
+        oos.writeObject(DirectorList.directorsList);
+        oos.close();
+    }
 }
